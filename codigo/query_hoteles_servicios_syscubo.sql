d8d5fd753696e8cdd9ba374db8b35dfe932a0156
Felipe Gerard <felipegerard@gmail.com>
2015-07-22 19:48:49 -0500

    Queries funcionales en Cubo + solicitud de version rapida

---
+++ codigo/query_hoteles_servicios_syscubo.sql
@@ -0,0 +1,137 @@
+
+DECLARE @agrup_cat TABLE (
+	Clav_Agrupador CHAR(2)
+	, Categoria_Alimentos CHAR(20)
+)
+
+DECLARE @rangos_agrup TABLE (
+	Clav_Agrupador CHAR(2)
+	, Rango_Agrupador INT
+)
+
+DECLARE @hot_serv TABLE (
+	Clav_Hotel INT
+	, Clav_Servicio VARCHAR(20)
+)
+
+DECLARE @listaHoteles AS TABLE (
+	Clav_Hotel INT
+)
+
+-- Lista de hoteles que queremos (por geografía, etc)
+INSERT INTO @listaHoteles
+EXEC spRM_ObtenerListaDeHoteles
+
+-- Prioridades de los agrupadores de planes (más es mejor)
+INSERT INTO @rangos_agrup VALUES
+('AI', 4)
+,('BB', 1)
+,('RO', 0)
+,('MD', 4)
+,('SA', 4)
+,('CP', 1)
+,('DB', 1)
+,('AG', 4)
+,('FP', 3)
+,('MP', 2)
+,('BD', 2)
+,('GA', 0)
+,('ND', 0)
+
+-- Tabla auxiliar para generar servicios de planes de alimentos
+INSERT INTO @agrup_cat VALUES
+('AI', 'BREAKFAST_INCLUDED')
+,('AI', 'LUNCH_INCLUDED')
+,('AI', 'DINNER_INCLUDED')
+,('AI', 'BEVERAGES_INCLUDED')
+,('BB', 'BREAKFAST_INCLUDED')
+,('MD', 'BREAKFAST_INCLUDED')
+,('MD', 'LUNCH_INCLUDED')
+,('MD', 'DINNER_INCLUDED')
+,('MD', 'BEVERAGES_INCLUDED')
+,('SA', 'BREAKFAST_INCLUDED')
+,('SA', 'LUNCH_INCLUDED')
+,('SA', 'DINNER_INCLUDED')
+,('SA', 'BEVERAGES_INCLUDED')
+,('CP', 'BREAKFAST_INCLUDED')
+,('DB', 'BREAKFAST_INCLUDED')
+,('AG', 'BREAKFAST_INCLUDED')
+,('AG', 'LUNCH_INCLUDED')
+,('AG', 'DINNER_INCLUDED')
+,('AG', 'BEVERAGES_INCLUDED')
+,('FP', 'BREAKFAST_INCLUDED')
+,('FP', 'LUNCH_INCLUDED')
+,('FP', 'DINNER_INCLUDED')
+,('MP', 'BREAKFAST_INCLUDED')
+,('MP', 'DINNER_INCLUDED')
+,('BD', 'BREAKFAST_INCLUDED')
+,('BD', 'DINNER_INCLUDED')
+
+-- Generamos los servicios de plan de alimentos y los insertamos en @hot_serv
+INSERT INTO @hot_serv
+SELECT
+	Clav_Hotel
+	, Categoria_Alimentos AS Clav_Servicio
+FROM (
+	SELECT
+		hct.Clav_Hotel
+		, p.Clav_Agrupador
+		, pa.Nombre_Agrupador
+		, ra.Rango_Agrupador
+		, ROW_NUMBER() 
+				over (Partition BY hct.Clav_Hotel
+					ORDER BY ra.Rango_Agrupador DESC )
+			rango
+	FROM dbo.hoteles_cuartos_Tarifas2 hct with (nolock)
+		INNER JOIN dbo.Planes p with (nolock)
+			ON hct.Clav_Plan = p.Clav_Plan
+		INNER JOIN dbo.Planes_Agrupadores pa with (nolock)
+			ON p.Clav_Agrupador = pa.Clav_Agrupador
+		LEFT JOIN @rangos_agrup ra
+			ON pa.Clav_Agrupador = ra.Clav_Agrupador
+	GROUP BY hct.Clav_Hotel
+		, p.Clav_Agrupador
+		, pa.Nombre_Agrupador
+		, ra.Rango_Agrupador
+	) t1
+	INNER JOIN @agrup_cat ac
+		ON t1.Clav_Agrupador = ac.Clav_Agrupador
+WHERE t1.rango = 1
+ORDER BY t1.Clav_Hotel
+
+-- Insertamos los demás servicios (provenientes de Hoteles_Servicios) en @hot_serv
+INSERT INTO @hot_serv
+SELECT
+	hs.Clav_Hotel,
+	hs.Clav_Servicio
+FROM dbo.Hoteles_Servicios hs WITH (NOLOCK)
+-- Agregar esto cuando esté la tabla de servicios en cubo
+/*	INNER JOIN dbo.Servicios s WITH (NOLOCK)
+		ON hs.Clav_Servicio = s.Clav_Servicio
+WHERE s.esCasa = 1
+--ORDER BY s.Orden
+*/
+
+-- Regresamos el resultado
+SELECT
+	hs.Clav_Hotel
+	, tcs.Codigo AS Categoria
+	, count(tcs.Codigo) as Cantidad
+FROM @hot_serv hs
+	INNER JOIN @listaHoteles lh					-- Sólo de los hoteles obtenidos en la lista
+		ON hs.Clav_Hotel = lh.Clav_Hotel
+	INNER JOIN Clasificaciones_Servicios cs
+		ON hs.Clav_Servicio = cs.Clav_Servicio
+	INNER JOIN Tipos_Clasificaciones_Servicios tcs
+		ON cs.Clav_TipoClasificacionServicio = tcs.Clav_TipoClasificacionServicio
+WHERE tcs.Codigo <> 'ZIGNORE'					-- Categoria a ignorar
+GROUP BY hs.Clav_Hotel, tcs.Codigo
+ORDER BY hs.Clav_Hotel, tcs.Codigo
+
+--select * from Tipos_Clasificaciones_Servicios
+--select * from Clasificaciones_Servicios
+
+
+
+
+
---
+++ codigo/query_hoteles_syscubo.sql
@@ -0,0 +1,65 @@
+
+DECLARE @fecha_ini AS CHAR(8) = '20130618'
+--DECLARE @fecha_fin AS CHAR(8) = '20150617'    -- Quité la fecha final para que siempre tome hasta lo más nuevo.
+DECLARE @listaHoteles AS TABLE (
+	Clav_Hotel INT
+)
+DECLARE @precios AS TABLE (
+	Clav_Hotel INT
+	, Volumen_Noches INT
+	, Precio_Dlls DOUBLE PRECISION
+	, Utilidad_Dlls DOUBLE PRECISION
+)
+
+-- Lista de hoteles que queremos (por geografía, etc)
+INSERT INTO @listaHoteles
+EXEC spRM_ObtenerListaDeHoteles
+
+-- Precios de largo plazo de los hoteles
+INSERT INTO @precios
+SELECT
+	h.Clav_Hotel
+	, sum(rh.Noches) as Volumen_Noches
+	, CASE
+		WHEN sum(rh.Noches) = 0 THEN NULL
+		ELSE sum(rh.Importe_Tot_Dlls)/sum(rh.Noches)
+	END as Precio_Dlls
+	, CASE
+		WHEN sum(rh.Noches) = 0 THEN NULL
+		ELSE  sum(rh.Utilidad_Dlls)/sum(rh.Noches)
+	END as Utilidad_Dlls
+FROM dbo.Reservacion r with (nolock)
+	INNER JOIN dbo.res_Hotel rh with (nolock)
+		ON r.Clav_Res = rh.Clav_Res
+	INNER JOIN dbo.Hoteles h with (nolock)
+		ON rh.Clav_Hotel = h.Clav_Hotel
+WHERE R.fecha_venta >= @fecha_ini
+--and R.fecha_venta <= @fecha_fin
+and R.Email not like '%@bestday.com%'
+and R.Email not like '%@hoteldo.com%'
+and R.Email not like '%@e-travelsolution.com%'
+and R.status_reserva <> 'X'
+and R.status_pago='P'
+GROUP BY h.Clav_Hotel, h.longitude, h.latitude
+
+-- Pegar los precios a la lista de hoteles. Las coordenadas se pegan por separado porque hay hoteles con coordenadas pero sin precio
+SELECT
+	lh.Clav_Hotel
+	,h.longitude
+	, h.latitude
+	, p.Precio_Dlls
+	, p.Volumen_Noches
+	, p.Utilidad_Dlls
+FROM @listaHoteles lh
+	LEFT JOIN @precios p
+		ON lh.Clav_Hotel = p.Clav_Hotel
+	LEFT JOIN dbo.Hoteles h with (nolock)
+		ON lh.Clav_Hotel = h.Clav_Hotel
+ORDER BY Clav_Hotel
+
+
+
+
+
+
+
