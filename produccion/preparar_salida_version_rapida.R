5d7d77d5fde9b1602f316f6659de6ec0d6f007f5
Felipe Gerard <felipegerard@gmail.com>
2015-07-27 19:56:01 -0500

    Habia que poner desc(rank) en el top_n

--- produccion/preparar_salida_version_rapida.R
+++ produccion/preparar_salida_version_rapida.R
@@ -4,7 +4,7 @@ out <- recomendados %>%
   group_by(cl1) %>%
   mutate(nrec = sum(within_price)) %>%
   filter(((nrec >= max_num_recom) & within_price) | (nrec < max_num_recom)) %>%
-  top_n(n = max_num_recom, rank) %>%
+  top_n(n = max_num_recom, desc(rank)) %>%
   arrange(cl1, rank) %>%
   dplyr::select(Clav_Hotel=cl1, Clav_HotelRecomendacion=cl2) %>%
   mutate(Prioridad = row_number()) %>%
